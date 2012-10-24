8bc8dbec0f83e93cbc58d35326c27b789cf341f9
MisterG <mathieu.grzybek@gmail.com>
2012-10-24 17:30:00 +0200

    Adding comments Adding C++ function and Perl script to clean RDS 2.37 NSRLFile.txt from some syntax errors

--- rds_import.cpp
+++ rds_import.cpp
@@ -1,3 +1,30 @@
+/**
+ * Project: nsrl-toolkit
+ * File name: rds_import.cpp
+ * Description: describes the main source file of rds_import
+ *
+ * @author Mathieu Grzybek on 2012-10-24
+ * @copyright 2012 Mathieu Grzybek. All rights reserved.
+ * @version $Id: code-gpl-license.txt,v 1.2 2004/05/04 13:19:30 garry Exp $
+ *
+ * @see The GNU Public License (GPL) version 3 or higher
+ *
+ *
+ * forensics-data-extractor is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
 #include "rds_import.h"
 
 void	usage() {
@@ -48,16 +75,25 @@ bool	burst_commit(QSqlDatabase& db, const uint burst) {
 
 bool	import_nsrl_file(QFile& q_stdin, QSqlDatabase& db, QSqlQuery& query, const uint burst) {
 	db.transaction();
+	uint	line_counter = 0;
 
 	while ( not q_stdin.atEnd() ) {
 		QString line = q_stdin.readLine().simplified();
 		QString	sql;
 		QString buffer;
 
+		// Skip useless lines
+		if ( line.indexOf(",") == -1 )
+			continue;
+
 		if ( line.length() > 1 ) {
 			QStringList	fields;
+			clean_nsrl_file_line(line);
 
-//			std::cout << "line: " << line.toStdString() << std::endl;
+			line_counter++;
+
+			std::cout << "line: " << line.toStdString() << std::endl;
+
 			/*
 			 * Let's extract the first five field ourselves
 			 * We could use regexp instead
@@ -67,18 +103,33 @@ bool	import_nsrl_file(QFile& q_stdin, QSqlDatabase& db, QSqlQuery& query, const 
 			buffer = line.left(line.indexOf('"'));
 			line.remove(0, line.indexOf('"') + 3);
 //			std::cout << "sha1: " << buffer.toStdString() << std::endl << "line: " << line.toStdString() << std::endl;
+			if ( buffer.size() != 40 ) {
+				std::cerr << "line: " << line_counter << std::endl;
+				std::cerr << "sha1's length is not 40 characters!" << std::endl;
+				return false;
+			}
 			fields << buffer;
 
 			// md5
 			buffer = line.left(line.indexOf('"'));
 			line.remove(0, line.indexOf('"') + 3);
 //			std::cout << "md5: " << buffer.toStdString() << std::endl << "line: " << line.toStdString() << std::endl;
+			if ( buffer.size() != 32 ) {
+				std::cerr << "line: " << line_counter << std::endl;
+				std::cerr << "md5's length is not 32 characters!" << std::endl;
+				return false;
+			}
 			fields << buffer;
 
 			// crc32
 			buffer = line.left(line.indexOf('"'));
 			line.remove(0, line.indexOf('"') + 3);
 //			std::cout << "crc32: " << buffer.toStdString() << std::endl << "line: " << line.toStdString() << std::endl;
+			if ( buffer.size() != 8 ) {
+				std::cerr << "line: " << line_counter << std::endl;
+				std::cerr << "crc's length is not 8 characters!" << std::endl;
+				return false;
+			}
 			fields << buffer;
 
 			// file_name
@@ -147,6 +198,7 @@ bool	import_nsrl_file(QFile& q_stdin, QSqlDatabase& db, QSqlQuery& query, const 
 //			std::cout << sql.toStdString() << std::endl;
 
 			if ( query.exec(sql) == false ) {
+				std::cerr << "fields size: " << fields.size() << std::endl;
 				std::cerr << "query: " << sql.toStdString() << std::endl;
 				std::cerr << "exec error: " << query.lastError().text().toStdString() << std::endl;
 				return false;
@@ -165,6 +217,26 @@ bool	import_nsrl_file(QFile& q_stdin, QSqlDatabase& db, QSqlQuery& query, const 
 	}
 
 	return true;
+}
+
+void	clean_nsrl_file_line(QString& line) {
+	QRegExp	col_1_match("^.{43}(.)");
+	QRegExp	col_1_replace("^(.{41}).{3}/$1\",\"");
+
+	QRegExp	col_2_match("^.{78}(.)");
+	QRegExp	col_2_replace("^(.{76}).{3}/$1\",\"");
+
+	QRegExp	col_3_match("^.{88}(.)");
+	QRegExp	col_3_replace("^(.{86}).{3}/$1\",\"");
+
+	if ( line.contains(col_1_match) == true )
+		line.replace(col_1_replace);
+
+	if ( line.contains(col_2_match) == true )
+		line.replace(col_2_replace);
+
+	if ( line.contains(col_3_match) == true )
+		line.replace(col_3_replace);
 }
 
 bool	import_nsrl_mfg(QFile& q_stdin, QSqlDatabase& db, QSqlQuery& query, const uint burst) {
--- rds_import.h
+++ rds_import.h
@@ -1,11 +1,36 @@
+/**
+ * Project: nsrl-toolkit
+ * File name: rds_import.h
+ * Description: describes the main source file of rds_import
+ *
+ * @author Mathieu Grzybek on 2012-10-24
+ * @copyright 2012 Mathieu Grzybek. All rights reserved.
+ * @version $Id: code-gpl-license.txt,v 1.2 2004/05/04 13:19:30 garry Exp $
+ *
+ * @see The GNU Public License (GPL) version 3 or higher
+ *
+ *
+ * forensics-data-extractor is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
 #include <cstdlib>
 #include <iostream>
 #include <stdio.h>
 
 #include <QStringBuilder>
 #include <QtSql>
-
-void	usage();
 
 /*
  * Database
@@ -21,6 +46,16 @@ bool	burst_commit(QSqlDatabase& db, uint burst);
 // Last to import
 bool	import_nsrl_file(QFile& q_stdin, QSqlDatabase& db, QSqlQuery& query, uint burst);
 
+/*
+ * clean_nsrl_file_line
+ *
+ * Puts the missing commas (RDS 237 contains "ere" strings instead of '","')
+ *
+ * @arg	line	:	 the line to clean
+ *
+ */
+void	clean_nsrl_file_line(QString& line);
+
 // First to import
 bool	import_nsrl_mfg(QFile& q_stdin, QSqlDatabase& db, QSqlQuery& query, uint burst);
 
@@ -35,4 +70,22 @@ bool	import_nsrl_prod(QFile& q_stdin, QSqlDatabase& db, QSqlQuery& product_query
  */
 
 int		main(int argc, char* argv[]);
+
+/*
+ * check_settings
+ *
+ * Deals with the saved settings according to the platform (using QSettings):
+ * - hostname:	the remote database server
+ * - database:	the name of the schema to use
+ * - user:		the username
+ * - burst:		the number of queries to commit at each commit
+ */
 void	check_settings();
+
+/*
+ * usage
+ *
+ * Prints the usage message to stdout
+ */
+void	usage();
+
