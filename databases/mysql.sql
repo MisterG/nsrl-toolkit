60c36e1676d6f9af2a2b4bb132a6492d16b91722
Mathieu Grzybek <mathieu.grzybek@gendarmerie.interieur.gouv.fr>
2013-03-05 08:34:16 +0100

    MySQL updates
    Switched file_size from INT(10) to BIGINT

--- databases/mysql.sql
+++ databases/mysql.sql
@@ -1,3 +1,5 @@
+BEGIN;
+
 DROP TABLE IF EXISTS hash;
 DROP TABLE IF EXISTS file;
 DROP TABLE IF EXISTS product_has_os;
@@ -15,14 +17,22 @@ CREATE TABLE IF NOT EXISTS `mfg` (
   PRIMARY KEY (`code`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+CREATE  TABLE IF NOT EXISTS `hash` (
+  `sha1` BINARY(40) NOT NULL ,
+  `md5` BINARY(32) NOT NULL ,
+  `crc32` VARCHAR(8) NULL ,
+  INDEX `hash_index_sha1` (`sha1` ASC) )
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
 CREATE  TABLE IF NOT EXISTS `file` (
   `file_name` VARCHAR(256) NOT NULL ,
-  `file_size` INT(10) UNSIGNED NOT NULL ,
-  `product_code` INT(10) UNSIGNED NOT NULL ,
+  `file_size` BIGINT UNSIGNED NOT NULL ,
+  `product_code` INTEGER UNSIGNED NOT NULL ,
   `op_system_code` VARCHAR(15) NOT NULL ,
   `special_code` VARCHAR(10) NOT NULL ,
   `hash_sha1` BINARY(40) NOT NULL ,
-  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT ,
+  `id` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT ,
   INDEX `file_index_product_code` (`product_code` ASC) ,
   INDEX `file_index_op_system_code` (`op_system_code` ASC) ,
   INDEX `file_index_special_code` (`special_code` ASC) ,
@@ -62,12 +72,12 @@ ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
 CREATE  TABLE IF NOT EXISTS `product` (
-  `product_code` INT(10) UNSIGNED NOT NULL ,
-  `product_name` VARCHAR(138) NOT NULL ,
+  `product_code` INTEGER UNSIGNED NOT NULL ,
+  `product_name` VARCHAR(150) NOT NULL ,
   `product_version` VARCHAR(49) NOT NULL ,
   `mfg_code` VARCHAR(45) NOT NULL ,
-  `language` VARCHAR(64) NOT NULL ,
-  `application_type` VARCHAR(36) NOT NULL ,
+  `language` VARCHAR(256) NOT NULL ,
+  `application_type` VARCHAR(128) NOT NULL ,
   PRIMARY KEY (`product_code`) ,
   INDEX `product_mfg_code_index` (`mfg_code` ASC) ,
   CONSTRAINT `product_fk_mfg`
@@ -79,7 +89,7 @@ ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
 CREATE  TABLE IF NOT EXISTS `product_has_os` (
-  `product_code` INT(10) UNSIGNED NOT NULL ,
+  `product_code` INTEGER UNSIGNED NOT NULL ,
   `system_code` VARCHAR(16) NOT NULL ,
   PRIMARY KEY (`product_code`, `system_code`) ,
   INDEX `fk_product_has_os_os1` (`system_code` ASC) ,
@@ -97,14 +107,9 @@ CREATE  TABLE IF NOT EXISTS `product_has_os` (
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
-CREATE  TABLE IF NOT EXISTS `hash` (
-  `sha1` BINARY(40) NOT NULL ,
-  `md5` BINARY(32) NOT NULL ,
-  `crc32` VARCHAR(8) NULL ,
-  INDEX `hash_index_sha1` (`sha1` ASC) )
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
-
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
+COMMIT;
+
